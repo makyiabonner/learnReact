804a8708f9fb67f6659021fc9c2a6d8bfadc9ebc
makyia bonner <makyiabonner@gmail.com>
2023-03-11 19:38:39 -0500

    Learning ReactDOM.render() and general easy setup

---
+++ Static Project Pages/ReactFacts/index.js
@@ -0,0 +1,6 @@
+ReactDOM.render(<ul>
+                    <li>Pepe</li>
+                    <li> The Frog</li>
+                    <li> Pele</li>
+                    <li> The Brasilian GOAT</li>
+                </ul>, document.querySelector('.root'))
